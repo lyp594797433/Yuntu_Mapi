b52e216499790ffa9867cc049b0a573aef084ee5
Administrator <594797433@qq.com>
2018-12-06 18:11:31 +0800

    Initial commit

---
+++ runner.py
@@ -0,0 +1,66 @@
+# -*- coding=utf-8 -*-
+import utils,time,log,re,sys,json,simplejson,random
+obj_log = log.get_logger()
+
+class Runner:
+	def __init__(self, all_config):
+		self.obj_tools = utils.Tools()
+		# self.hallCode =  self.obj_utils.hallCode
+		self.user = all_config['username']
+		self.pwd = all_config['pwd']
+		self.port = all_config['port']
+		self.ip = all_config['ip']
+		self.my_user1 = all_config['my_user1']
+		self.my_pwd1 = all_config['my_pwd1']
+		self.add = self.obj_tools.add
+	def _get_libraryuser_info(self,hallCode):
+		'''获取图书馆admin账号密码
+		:param hallCode : 图书馆代码
+		:rtype : dict
+		'''
+		sql_statement = '''SELECT id,
+							userName,
+							password
+						FROM
+							librarys_users
+						WHERE
+							libraryHallCode = "''' +  hallCode + '''" AND isManager = 1
+							AND isEffective = 1'''
+		print(sql_statement)
+		sql_rtn = self.obj_tools.sql_event(sql_statement)
+		return sql_rtn[0]
+
+	def _get_librarybooks(self,hallCode,bookState=1,byHallCode=1,storeroom=2):
+		'''获取图书馆图书.
+		:param hallCode : 图书馆代码
+		:param byHallCode : 本馆： 1，异馆：馆号
+		:param bookState : 图书状态 1：在馆 2：在借 and so on
+		:rtype : dict
+		'''
+		REQ_TYPE = 'GET'
+		temp_dict = {}
+		books_list = []
+		full_barnumber_list = []
+		temp_dict['byHallCode'] = byHallCode
+		temp_dict['bookState'] = bookState
+		temp_dict['hallCode'] = hallCode
+		temp_dict['storeroom'] = storeroom
+		API_URL = "http://" + self.add + "/api/statistics/getLibraryStatics"
+		get_user_info = self._get_libraryuser_info(hallCode)
+		get_user = get_user_info['userName']
+		get_pwd = get_user_info['password']
+		token = self.obj_tools.loginYuntu(hallCode, get_user, get_pwd)
+		req = self.obj_tools.call_rest_api(API_URL, REQ_TYPE,data_rtn=temp_dict, token=token)
+		if req['status'] == 200:
+			obj_log.info('获取图书馆:{}书籍成功........'.format(hallCode))
+			books_list = req['data']['resultList']
+			for temp in books_list:
+				full_barnumber_temp = temp["belongLibraryHallCode"] + "-"  + temp["barNumber"]
+				full_barnumber_list.append(full_barnumber_temp)
+			return  full_barnumber_list
+		else:
+			obj_log.info('获取图书馆:{}书籍失败........'.format(hallCode))
+			return False
+
+		return first_category_rtn
+
---
+++ yuntucase.py
@@ -0,0 +1,47 @@
+# -*- coding=utf-8 -*-
+import sys,utils,unittest,runner,log,time,HTMLTestRunner,os
+from test_case import Test_case
+import test_case
+from selenium import webdriver
+to_list = ["18782019436@163.com"]
+obj_log = log.get_logger()
+log_file = (os.path.split(os.path.realpath(__file__)))[0] + '\\' + 'log\yuntu.log'
+fp = open(log_file,'w')
+class Yuntu_case(unittest.TestCase):
+	@classmethod
+	def setUp(self):
+		self.obj_test_case = Test_case(all_config)
+	@classmethod
+	def tearDown(self):
+		# driver.close()
+		pass
+	def books_borrow(self):
+		obj_log.info('Add new start................')
+		self.assertEqual(self.obj_test_case.borrow_books("AABCE"), True)
+
+def suite():
+	suite = unittest.TestSuite()
+	suite.addTest(Yuntu_case("books_borrow"))
+	return suite
+
+if __name__ == '__main__':
+	obj_tools = utils.Tools()
+	all_config = obj_tools.all_config
+
+	'''测试报告生成'''
+	now_time = time.strftime("%Y%m%d%H%M", time.localtime(time.time()))
+	# filename = (os.path.split(os.path.realpath(__file__)))[0] + '\\Report\\'+ now_time  + r'testReport.html'
+	# fip = open(filename, 'wb')
+	# unitrunner = HTMLTestRunner.HTMLTestRunner(
+	# 	stream=fip,
+	# 	title=u'云图测试报告',
+	# 	description=u'测试用例详细信息'
+	# )
+	unitrunner = unittest.TextTestRunner(fp)
+
+	rtn1 = unitrunner.run(suite())
+	fp.close()
+	# html_report = fip.name
+	'''邮件发送'''
+	# sendmail = obj_tools.send_mail(to_list,html_report)
+	print('Test Result:', rtn1)
